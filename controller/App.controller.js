58f30bcbed0b88aec4086cefcbded0f09df92c41
Pablo Casado <pablo.casado@dormakaba.com>
2020-05-14 10:31:57 +0000

    Updates files ans estructure

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define(
 	[
-		'sap/ui/core/mvc/Controller',
-		'sap/ui/model/json/JSONModel',
-		'sap/m/MessageToast',
+		//'sap/ui/core/mvc/Controller',
+		//'sap/ui/model/json/JSONModel',
+		'Calculator/controller/BaseController',
+		'sap/m/MessageToast'
 	],
-	function (Controller, JSONModel, MessageToast) {
+	function (BController, MessageToast) {
 		'use strict';
-		return Controller.extend('Calculator.controller.App', {
+		return BController.extend('Calculator.controller.App', {
 			onInit: function () {
 				//this.byId("Importe").setValue(100);
 				this.byId('Ticket1').setValue(5);
@@ -18,52 +19,8 @@ sap.ui.define(
 				this.byId('ready').setVisible(bState);
 			},
 
-			findSolution: function (target, vtick1, vtick2) {
-				let coin = 1; //Monedas sueltas
-				function find(current, qtick1, qtick2, coins) {
-					if (current == target) {
-						return (
-							`Tickets de ${vtick1}€ :` +
-							`${qtick1}, de ${vtick2}€ :` +
-							qtick2 +
-							`. Suelto ${coins}€`
-						);
-					} else if (current > target) {
-						return null;
-					} else {
-						return (
-							find(current + vtick1, qtick1 + 1, qtick2, coins) ||
-							find(current + vtick2, qtick1, qtick2 + 1, coins) ||
-							find(current + coin, qtick1, qtick2, coins + 1)
-						);
-					}
-				}
-				return find(0, 0, 0, 0);
-			},
-
-			findSolution2: function (target, vtick1, vtick2) {
-				let coin = 1; //Monedas sueltas
-				function find(current, qtick1, qtick2, coins) {
-					if (current == target) {
-						return `Tickets de ${vtick1}€ :` + `${qtick1}, de ${vtick2}€ :` + qtick2 + `. Suelto ${coins}€`;
-					} else if (current > target) {
-						return null;
-					} else {
-						if (qtick1 > qtick2) {
-							return find(current + vtick2, qtick1, qtick2 + 1, coins) ||
-								find(current + vtick1, qtick1 + 1, qtick2, coins) ||
-								find(current + coin, qtick1, qtick2, coins + 1);
-						} else {
-
-							return find(current + vtick1, qtick1 + 1, qtick2, coins) ||
-								find(current + vtick2, qtick1, qtick2 + 1, coins) ||
-								find(current + coin, qtick1, qtick2, coins + 1);
-						}
-					}
-				}
-				return find(0, 0, 0, 0);
-			},
-
+		
+ 
 			onCalculate: function (oEvent) {
 				//tomar valores
 				let iImporte = 0,
---
+++ controller/BaseController.js
@@ -0,0 +1,53 @@
+	sap.ui.define([
+		"sap/ui/core/mvc/Controller",
+		"sap/ui/core/routing/History"
+	], function (Controller, History) {
+	"use strict";
+	return Controller.extend("Calculator.controller.BaseController",{
+	findSolution: function (target, vtick1, vtick2) {
+				let coin = 1; //Monedas sueltas
+				function find(current, qtick1, qtick2, coins) {
+					if (current == target) {
+						return (
+							`Tickets de ${vtick1}€ :` +
+							`${qtick1}, de ${vtick2}€ :` +
+							qtick2 +
+							`. Suelto ${coins}€`
+						);
+					} else if (current > target) {
+						return null;
+					} else {
+						return (
+							find(current + vtick1, qtick1 + 1, qtick2, coins) ||
+							find(current + vtick2, qtick1, qtick2 + 1, coins) ||
+							find(current + coin, qtick1, qtick2, coins + 1)
+						);
+					}
+				}
+				return find(0, 0, 0, 0);
+			},
+
+			findSolution2: function (target, vtick1, vtick2) {
+				let coin = 1; //Monedas sueltas
+				function find(current, qtick1, qtick2, coins) {
+					if (current == target) {
+						return `Tickets de ${vtick1}€ :` + `${qtick1}, de ${vtick2}€ :` + qtick2 + `. Suelto ${coins}€`;
+					} else if (current > target) {
+						return null;
+					} else {
+						if (qtick1 > qtick2) {
+							return find(current + vtick2, qtick1, qtick2 + 1, coins) ||
+								find(current + vtick1, qtick1 + 1, qtick2, coins) ||
+								find(current + coin, qtick1, qtick2, coins + 1);
+						} else {
+
+							return find(current + vtick1, qtick1 + 1, qtick2, coins) ||
+								find(current + vtick2, qtick1, qtick2 + 1, coins) ||
+								find(current + coin, qtick1, qtick2, coins + 1);
+						}
+					}
+				}
+				return find(0, 0, 0, 0);
+			}
+	});
+});
