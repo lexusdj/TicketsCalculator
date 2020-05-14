2b4d556c8b61e04783d50f6dbd85e5feda9219c7
Pablo Casado <pablo.casado@dormakaba.com>
2020-05-14 13:25:06 +0000

    otra mejora

--- Component.js
+++ Component.js
@@ -1,15 +1,15 @@
 sap.ui.define([
-		"sap/ui/core/UIComponent",
-		"sap/ui/model/resource/ResourceModel",
-		"sap/ui/model/json/JSONModel"
-	], function (UIComponent, ResourceModel, JSONModel) {
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/resource/ResourceModel",
+	"sap/ui/model/json/JSONModel"
+], function (UIComponent, ResourceModel, JSONModel) {
 	"use strict";
 
 	return UIComponent.extend("Calculator.Component", {
 
 		metadata: {
 			manifest: "json"
-		 },
+		},
 
 		/**
 		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
@@ -17,7 +17,7 @@ sap.ui.define([
 		 * @public
 		 * @override
 		 */
-		init : function () {
+		init: function () {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
